9c71c74623a75e6eb6f44ac38c46a8731471f01d
Artem <artem-orlov92@bk.ru>
2023-04-27 00:36:04 +0300

    Initial Commit

---
+++ NewsAppTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppTestTask
+//
+//  Created by Артем Орлов on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
